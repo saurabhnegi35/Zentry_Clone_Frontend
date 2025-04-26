2e73913353db068b96485cfc69ef7aadd161d5c0
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2025-04-26 11:36:59 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
   //   console.log(currentIndex);
